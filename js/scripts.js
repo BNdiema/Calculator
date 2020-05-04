0ca320e98b5a397bbbe8d2013984e80cc9b6248c
Brian <bryantndiema@gmail.com>
2020-05-04 11:53:01 +0300

    add a multiplication code to script.js file

--- js/scripts.js
+++ js/scripts.js
@@ -15,3 +15,12 @@ var subtract = function(number1, number2) {
     var number2 = parseInt(prompt("Enter another number:"));
     var result = subtract(number1, number2);
     alert(result);
+
+    var multiplication = function(number1, number2) {
+        return number1 * number2;
+        };
+        
+        var number1 = parseInt(prompt("Enter a number:"));
+        var number2 = parseInt(prompt("Enter another number:"));
+        var result = multiplication(number1, number2);
+        alert(result);
